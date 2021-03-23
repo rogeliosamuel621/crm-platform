b69c24f397ce1173b52cb07b8bed1a6851136f13
Angel Martinez <angel.martineztrc@gmail.com>
2021-03-22 20:19:48 -0600

    feat(api/products): :sparkles:  adding schema for products

---
+++ packages/api/src/product/schemas/product.schema.ts
@@ -0,0 +1,25 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: String, default: 'N/A' })
+  description: string;
+
+  @Prop({ type: Number, required: true })
+  price: number;
+
+  @Prop({ type: Number, required: true })
+  stock: number;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true })
+  owner: mongoose.Types.ObjectId;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
