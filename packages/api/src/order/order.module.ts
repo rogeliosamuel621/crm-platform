4d79bfb8055d27e82bb669863463f90cc088441e
Angel Martinez <angel.martineztrc@gmail.com>
2021-03-23 23:14:53 -0600

    feat(api/orders): :sparkles:  adding order schema

--- packages/api/src/order/order.module.ts
+++ packages/api/src/order/order.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AuthenticationModule } from 'src/authentication/authentication.module';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { Order, OrderSchema } from './schemas/order.schema';
 
 @Module({
+  imports: [
+    AuthenticationModule,
+    MongooseModule.forFeature([
+      {
+        name: Order.name,
+        schema: OrderSchema,
+      },
+    ]),
+  ],
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService],
 })
 export class OrderModule {}
---
+++ packages/api/src/order/schemas/order.schema.ts
@@ -0,0 +1,47 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
+
+export type OrderDocument = Order & Document;
+
+@Schema()
+export class Order {
+  @Prop({ type: String, required: true })
+  name: string;
+
+  @Prop({ type: Number, min: 0, required: true })
+  total: number;
+
+  @Prop({
+    type: String,
+    default: 'PENDING',
+    enum: ['PENDING', 'IN PROGRESS', 'COMPLETED'],
+  })
+  status: string;
+
+  // I DONT KNOW IF THIS THING WORKS
+  @Prop([
+    { type: Number, required: true, min: 0 },
+    { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+  ])
+  products: [{ quantity: number; product: mongoose.Types.ObjectId }];
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Customer',
+    required: true,
+  })
+  customer: mongoose.Types.ObjectId;
+
+  @Prop({
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  })
+  owner: mongoose.Types.ObjectId;
+
+  @Prop({ type: Date, default: Date.now() })
+  createdAt: Date;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(Order);
