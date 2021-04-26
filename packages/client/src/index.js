13b87eee63e49d37f3c5345a0751a63cc28607ca
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-26 18:43:24 -0500

    feat(client):  add the windmill dashboard

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,12 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React, { Suspense } from 'react'
+import ReactDOM from 'react-dom'
+import './assets/css/tailwind.output.css'
+import App from './App'
+import { SidebarProvider } from './context/SidebarContext'
+import ThemedSuspense from './components/ThemedSuspense'
+import { Windmill } from '@windmill/react-ui'
+import * as serviceWorker from './serviceWorker'
 
-import './index.css';
+// if (process.env.NODE_ENV !== 'production') {
+//   const axe = require('react-axe')
+//   axe(React, ReactDOM, 1000)
+// }
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <SidebarProvider>
+    <Suspense fallback={<ThemedSuspense />}>
+      <Windmill usePreferences>
+        <App />
+      </Windmill>
+    </Suspense>
+  </SidebarProvider>,
   document.getElementById('root')
-);
+)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register()
