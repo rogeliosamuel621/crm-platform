0a81c576189f032955a185a3baad06d08c0302b8
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-26 18:52:58 -0500

    refactor(client):  add a better way to use tailwindcss

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,16 +1,12 @@
-import React, { Suspense } from 'react'
-import ReactDOM from 'react-dom'
-import './assets/css/tailwind.output.css'
-import App from './App'
-import { SidebarProvider } from './context/SidebarContext'
-import ThemedSuspense from './components/ThemedSuspense'
-import { Windmill } from '@windmill/react-ui'
-import * as serviceWorker from './serviceWorker'
+import React, { Suspense } from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { SidebarProvider } from './context/SidebarContext';
+import ThemedSuspense from './components/ThemedSuspense';
+import { Windmill } from '@windmill/react-ui';
 
-// if (process.env.NODE_ENV !== 'production') {
-//   const axe = require('react-axe')
-//   axe(React, ReactDOM, 1000)
-// }
+// tailwindcss
+import './assets/css/tailwind.css';
 
 ReactDOM.render(
   <SidebarProvider>
@@ -21,9 +17,4 @@ ReactDOM.render(
     </Suspense>
   </SidebarProvider>,
   document.getElementById('root')
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+);
