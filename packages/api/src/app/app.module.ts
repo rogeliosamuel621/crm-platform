d1411830108da1ec4eac2b1f7f965ac1e360ecb4
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-29 00:09:46 -0500

    feat(api/config):  set up the config module

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,11 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
+import configObject from '../config/config-object';
+import configSchema from '../config/config-schema';
+
 import { UsersModule } from './users/users.module';
 import { CustomersModule } from './customers/customers.module';
 import { OrdersModule } from './orders/orders.module';
 import { ProductsModule } from './products/products.module';
-import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [UsersModule, CustomersModule, OrdersModule, ProductsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configObject],
+      validationSchema: configSchema,
+    }),
+    UsersModule,
+    CustomersModule,
+    OrdersModule,
+    ProductsModule,
+  ],
 })
 export class AppModule {}
--- packages/api/src/main.ts
+++ packages/api/src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { Logger } from '@nestjs/common';
+
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+
+  const port: number = config.get<number>('port');
+  await app.listen(port, () => {
+    Logger.log(`Listening at http://localhost:${port}/`);
+  });
 }
 bootstrap();
