0607319a5db380515b4a8e0ac52faf7601d52746
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-29 00:29:29 -0500

    feat(api/users):  add users schema

--- packages/api/src/app/users/entities/user.entity.ts
+++ packages/api/src/app/users/entities/user.entity.ts
@@ -1,1 +1,29 @@
-export class User {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class User {
+  // TODO: field for the user's picture
+
+  @Prop({ type: String, trim: true, required: true })
+  name: string;
+
+  @Prop({ type: String, trim: true, required: true })
+  username: string;
+
+  @Prop({ type: String, unique: true, trim: true, required: true })
+  email: string;
+
+  @Prop({ type: String, trim: true, required: true })
+  password: string;
+
+  @Prop({ type: Boolean, default: true })
+  enabled: boolean;
+
+  @Prop({ type: Date, default: Date.now() })
+  createdAt: Date;
+}
+
+export type UserDocument = User & Document;
+
+export const UserSchema = SchemaFactory.createForClass(User);
