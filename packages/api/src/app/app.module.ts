d4b811357ac7e84bfb935a053d9ae5af4fca9f88
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-28 23:49:57 -0500

    feat(api/app):  generate users, products, orders and customers modules

--- packages/api/src/app/app.module.ts
+++ packages/api/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { CustomersModule } from './customers/customers.module';
+import { OrdersModule } from './orders/orders.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CustomersModule, OrdersModule, ProductsModule],
 })
 export class AppModule {}
