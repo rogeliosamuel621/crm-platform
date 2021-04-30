729d5c3e12587f3885205e00aef8031081cb0840
Angel Martinez <angel.martineztrc@gmail.com>
2021-04-30 14:20:14 -0500

    feat(api/customers):  add customers entity

--- packages/api/src/app/customers/customers.module.ts
+++ packages/api/src/app/customers/customers.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { CustomersService } from './customers.service';
 import { CustomersController } from './customers.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Customer, CustomerSchema } from './entities/customer.entity';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+        name: Customer.name,
+        schema: CustomerSchema
+      }
+    ])
+  ],
   controllers: [CustomersController],
   providers: [CustomersService]
 })
--- packages/api/src/app/customers/entities/customer.entity.ts
+++ packages/api/src/app/customers/entities/customer.entity.ts
@@ -1,1 +1,30 @@
-export class Customer {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document, Schema as MongooseSchema } from 'mongoose';
+import { User } from '../../users/entities/user.entity';
+
+@Schema()
+export class Customer {
+  _id: MongooseSchema.Types.ObjectId;
+
+  @Prop({ type: String, trim: true, required: true })
+  name: string;
+
+  @Prop({ type: String, trim: true, unique: true, required: true })
+  email: string;
+
+  @Prop({ type: String, trim: true, default: 'N/A' })
+  position?: string;
+
+  @Prop({ type: String, trim: true, default: 'N/A' })
+  company: string;
+
+  @Prop({ type: MongooseSchema.Types.ObjectId, ref: User.name, required: true })
+  owner: MongooseSchema.Types.ObjectId;
+
+  @Prop({ type: Date, default: Date.now() })
+  createdAt: Date;
+}
+
+export type CustomerDocument = Customer & Document;
+
+export const CustomerSchema = SchemaFactory.createForClass(Customer);
