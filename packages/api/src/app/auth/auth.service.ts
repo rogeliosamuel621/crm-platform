084c444aaf135af0b688b89b6b3e90a823bb5001
Angel Martinez <angel.martineztrc@gmail.com>
2021-05-02 17:20:07 -0500

    refactor(api/app):  better structure in services

--- packages/api/src/app/auth/auth.service.ts
+++ packages/api/src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'app/users/entities/user.entity';
 
@@ -17,39 +17,24 @@ export class AuthService {
   ) {}
 
   async authenticate(payload: AuthCredentialsDto): Promise<string> {
-    try {
-      // check if the user's credentials are valid
-      const { _id, username, email } = await this.usersService.validate(
-        payload
-      );
+    // check if the user's credentials are valid
+    const { _id, username, email } = await this.usersService.validate(payload);
 
-      // generate the token
-      const jwtPayload: JwtPayload = { id: `${_id}`, username, email };
-      return await this.jwtService.signAsync(jwtPayload);
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
-    }
+    // generate the token
+    const jwtPayload: JwtPayload = { id: `${_id}`, username, email };
+    return await this.jwtService.signAsync(jwtPayload);
   }
 
   async register(payload: RegisterUserDto): Promise<string> {
-    try {
-      // create an user and get the information
-      const { _id, username, email } = await this.usersService.create(payload);
+    // create an user and get the information
+    const { _id, username, email } = await this.usersService.create(payload);
 
-      // generate the token
-      const jwtPayload: JwtPayload = { id: `${_id}`, username, email };
-      return await this.jwtService.signAsync(jwtPayload);
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
-    }
+    // generate the token
+    const jwtPayload: JwtPayload = { id: `${_id}`, username, email };
+    return await this.jwtService.signAsync(jwtPayload);
   }
 
   async findCurrent(payload: string): Promise<User> {
-    try {
-      // find and return the current user
-      return await this.usersService.findOneById(payload);
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
-    }
+    return await this.usersService.findOneById(payload);
   }
 }
--- packages/api/src/app/users/users.service.ts
+++ packages/api/src/app/users/users.service.ts
@@ -2,7 +2,8 @@ import { Model } from 'mongoose';
 import {
   Injectable,
   BadRequestException,
-  InternalServerErrorException
+  InternalServerErrorException,
+  HttpException
 } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import * as argon2 from 'argon2';
@@ -23,50 +24,42 @@ export class UsersService {
   async create(payload: CreateUserDto): Promise<User> {
     const { email, password } = payload;
 
-    try {
-      // check if there is already a user with that email
-      const isEmailAlreadyInUse: User = await this.model.findOne({ email });
-      if (isEmailAlreadyInUse) {
-        throw new BadRequestException(
-          'An account is already registered with this email'
-        );
-      }
-
-      // create a hash for the user's password
-      const hashedPassword = await argon2.hash(password);
-
-      // create a new user object
-      const user = new this.model({
-        ...payload,
-        password: hashedPassword
-      });
-
-      return await user.save();
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
+    // check if there is already a user with that email
+    const isEmailAlreadyInUse: User = await this.model.findOne({ email });
+    if (isEmailAlreadyInUse) {
+      throw new BadRequestException(
+        'An account is already registered with this email'
+      );
     }
+
+    // create a hash for the user's password
+    const hashedPassword = await argon2.hash(password);
+
+    // create a new user object
+    const user = new this.model({
+      ...payload,
+      password: hashedPassword
+    });
+
+    return await user.save();
   }
 
   async validate(payload: ValidateUserDto): Promise<User> {
     const { email, password } = payload;
 
-    try {
-      // verify user exists
-      const user: User = await this.model.findOne({ email }).select('password');
-      if (!user) {
-        throw new BadRequestException('The entered credentials are incorrect');
-      }
-
-      // verify that the password is correct
-      const isPasswordCorrect = await argon2.verify(user.password, password);
-      if (!isPasswordCorrect) {
-        throw new BadRequestException('The entered credentials are incorrect');
-      }
-
-      return user;
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
+    // verify user exists
+    const user: User = await this.model.findOne({ email }).select('password');
+    if (!user) {
+      throw new BadRequestException('The entered credentials are incorrect');
     }
+
+    // verify that the password is correct
+    const isPasswordCorrect = await argon2.verify(user.password, password);
+    if (!isPasswordCorrect) {
+      throw new BadRequestException('The entered credentials are incorrect');
+    }
+
+    return user;
   }
 
   findAll() {
@@ -74,17 +67,13 @@ export class UsersService {
   }
 
   async findOneById(payload: string): Promise<User> {
-    try {
-      // find the user by their _id
-      const user: User = await this.model.findOne({ _id: payload });
-      if (!user) {
-        throw new NotFoundException('User not found');
-      }
-
-      return user;
-    } catch (error) {
-      throw new InternalServerErrorException(error.message);
+    // find the user by their _id
+    const user: User = await this.model.findOne({ _id: payload });
+    if (!user) {
+      throw new NotFoundException('User not found');
     }
+
+    return user;
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
